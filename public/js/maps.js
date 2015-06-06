57854a1015474f2ddd7d89be03e6913fd1234ff1
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-06-05 23:59:31 -0400

    worked some javascript magic to autogenerate map javascript stuff

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,26 +1,13 @@
-var map1 = L.map('map1').setView([33.469, -81.987], 13);
+// I honestly feel pretty badass for writing this.
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'jpsandlin.mcfo6ocn',
-    accessToken: 'pk.eyJ1IjoianBzYW5kbGluIiwiYSI6IkVOV2NvSXcifQ.sf9FqaVmH7rICIn0wwSlxw'
-}).addTo(map1);
+Array.prototype.forEach.call(document.querySelectorAll('.map'), function(map) {
+    console.log('found map!');
+    var mapBox = L.map(map.id).setView([33.469, -81.987], 13);
 
-var map2 = L.map('map2').setView([33.41, -82.318], 13);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'jpsandlin.mcfo6ocn',
-    accessToken: 'pk.eyJ1IjoianBzYW5kbGluIiwiYSI6IkVOV2NvSXcifQ.sf9FqaVmH7rICIn0wwSlxw'
-}).addTo(map2);
-
-var map3 = L.map('map3').setView([33.445, -82.205], 13);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'jpsandlin.mcfo6ocn',
-    accessToken: 'pk.eyJ1IjoianBzYW5kbGluIiwiYSI6IkVOV2NvSXcifQ.sf9FqaVmH7rICIn0wwSlxw'
-}).addTo(map3);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>, Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'jpsandlin.mcfo6ocn',
+        accessToken: 'pk.eyJ1IjoianBzYW5kbGluIiwiYSI6IkVOV2NvSXcifQ.sf9FqaVmH7rICIn0wwSlxw'
+    }).addTo(mapBox);
+});
